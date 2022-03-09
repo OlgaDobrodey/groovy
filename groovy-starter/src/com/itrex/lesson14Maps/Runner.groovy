bb569d6f77a200f0b764babd3efe5b75dd855d5e
OlgaDobrodey <olga.dobrodey@itrexgroup.com>
2022-03-09 12:52:21 +0300

    added Lesson14  Map Runner

--- groovy-starter/src/com/itrex/lesson14Maps/Runner.groovy
+++ groovy-starter/src/com/itrex/lesson14Maps/Runner.groovy
@@ -3,12 +3,12 @@ package com.itrex.lesson14Maps
 class Runner {
 
     static void main(String[] args) {
-        def map = ["one":1, "two":2]
+        def map = ["one": 1, "two": 2]
         //def map= [:] -empty map
 
-        assert  map.getClass() == LinkedHashMap
+        assert map.getClass() == LinkedHashMap
         assert map.one == 1 //invokeMissingProperty
-        assert map.get("qwe",10) == 10 //
+        assert map.get("qwe", 10) == 10 //
 
         //write
         map.one = 111
@@ -16,13 +16,10 @@ class Runner {
         println map //[one:111, two:2, qwe:10, three:123]
 
         //new method
-        assert map.subMap("one","two") ==  ['one':111, 'two':2]
+        assert map.subMap("one", "two") == ['one': 111, 'two': 2]
 
         //new operator
-        var newMap = ["q":22, "rt":99, *:map]
+        var newMap = ["q": 22, "rt": 99, *: map]
         println newMap //[q:22, rt:99, one:111, two:2, qwe:10, three:123]
-
-
-
     }
 }
